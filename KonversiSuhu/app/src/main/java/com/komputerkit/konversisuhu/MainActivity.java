ad8f3b43cab2da872530a95bd918737fe9db634e
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-07-30 20:40:12 +0700

    10. Android Studio Untuk Siswa SMK - Spinner Android Studio - Projek Konversi Suhu
    android studio

--- KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/komputerkit/konversisuhu/MainActivity.java
@@ -1,6 +1,9 @@
 package com.komputerkit.konversisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        load();
+        isiSpinner();
+    }
+
+    public void load(){
+        spinner=findViewById(R.id.spinner);
+    }
+
+    public void isiSpinner(){
+        String[] isi={"Celcius To Reamur","Celcius To Fahrenheit","Celcius To Kelvin"};
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+        spinner.setAdapter(adapter);
+    }
+
+    public void btnKonversi(View view){
+        String pilihan=spinner.getSelectedItem().toString();
+        System.out.println(pilihan);
     }
 }
