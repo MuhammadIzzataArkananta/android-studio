13860d963dcdf029f3de3b25dd921082ffc279af
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-08-03 15:16:52 +0700

    24. Android Studio Untuk Siswa SMK - Android RecyclerView - 5 - Menambahkan Menu Option
    android studio

--- RecyclerViewCardView/app/src/main/java/com/komputerkit/recyclerviewcardview/SiswaAdapter.java
+++ RecyclerViewCardView/app/src/main/java/com/komputerkit/recyclerviewcardview/SiswaAdapter.java
@@ -1,9 +1,12 @@
 package com.komputerkit.recyclerviewcardview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,15 +33,47 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        Siswa siswa = siswaList.get(i);
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, @SuppressLint("RecyclerView") final int i) {
+        final Siswa siswa = siswaList.get(i);
         viewHolder.tvNama.setText(siswa.getNama());
         viewHolder.tvAlamat.setText(siswa.getAlamat());
+
+        /*
 
         viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context, "Nama : "+siswa.getNama()+ ", Alamat : "+ siswa.getAlamat(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+         */
+
+        viewHolder.tvMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(context, viewHolder.tvMenu);
+                popupMenu.inflate(R.menu.menu_option);
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        int idItem = item.getItemId();
+                        if (idItem == R.id.menu_simpan) {
+                            Toast.makeText(context, "Simpan Data "+siswa.getNama(), Toast.LENGTH_SHORT).show();
+                        } else if (idItem == R.id.menu_hapus){
+                            siswaList.remove(i);
+                            notifyDataSetChanged();
+                            Toast.makeText(context, siswa.getNama()+" Sudah di Hapus", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                        return false;
+                    }
+                });
+
+                popupMenu.show();
             }
         });
     }
@@ -50,13 +85,14 @@ public class SiswaAdapter extends RecyclerView.Adapter<SiswaAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView tvNama, tvAlamat;
+        TextView tvNama, tvAlamat, tvMenu;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             tvNama = itemView.findViewById(R.id.tvNama);
             tvAlamat = itemView.findViewById(R.id.tvAlamat);
+            tvMenu = itemView.findViewById(R.id.tvMenu);
         }
     }
 }
