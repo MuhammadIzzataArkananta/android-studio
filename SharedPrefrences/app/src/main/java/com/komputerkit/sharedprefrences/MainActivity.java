bfdf2aa5279f7cc34fe2966a794f5ef8a99a3978
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-08-05 19:25:35 +0700

    39. Android Studio Untuk Siswa SMK Android SharedPreferences - Part 1
    android studio

---
+++ SharedPrefrences/app/src/main/java/com/komputerkit/sharedprefrences/MainActivity.java
@@ -0,0 +1,62 @@
+package com.komputerkit.sharedprefrences;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView etBarang, etStok;
+    SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        load();
+
+    }
+
+    public void load(){
+        etBarang = findViewById(R.id.etBarang);
+        etStok = findViewById(R.id.etStok);
+        sharedPreferences =getSharedPreferences("barang", MODE_PRIVATE);
+    }
+
+    public void isiSharedPrefrences(String barang, float stok){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("barang", barang);
+        editor.putFloat("stok", stok);
+        editor.apply();
+    }
+
+    public void simpan(View view) {
+        String barang = etBarang.getText().toString();
+        float stok = Float.parseFloat(etStok.getText().toString());
+
+        if (barang.isEmpty() || stok == 0.0){
+            Toast.makeText(this, "Data Kosong", Toast.LENGTH_SHORT).show();
+        }else {
+            isiSharedPrefrences(barang,stok);
+            Toast.makeText(this, "Data Sudah Di Simpan", Toast.LENGTH_SHORT).show();
+        }
+
+        etBarang.setText("");
+        etStok.setText("");
+    }
+
+    public void tampil(View view) {
+    }
+}
